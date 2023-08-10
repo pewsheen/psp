0b7d8c5f3805cb6d7a62fbb13cbc3646105f8e2c
Jason Tsai <jason@pews.dev>
2023-08-10 15:16:04 +0800

    fix(windows): check hwnd is valid

--- src/platform_impl/windows/power_monitor.rs
+++ src/platform_impl/windows/power_monitor.rs
@@ -1,6 +1,5 @@
 use crate::monitor::{PowerEventChannel, PowerState};
 use windows::{
-  core::Result,
   s,
   Win32::{
     Foundation::{HANDLE, HWND, LPARAM, LRESULT, WPARAM},
@@ -11,10 +10,9 @@ use windows::{
       SystemServices::GUID_POWERSCHEME_PERSONALITY,
     },
     UI::WindowsAndMessaging::{
-      CreateWindowExA, DefWindowProcA, LoadCursorW, RegisterClassA, CS_HREDRAW, CS_VREDRAW,
-      CW_USEDEFAULT, IDC_ARROW, PBT_APMRESUMESUSPEND, PBT_APMSUSPEND, WINDOW_EX_STYLE,
-      WM_POWERBROADCAST, WM_WTSSESSION_CHANGE, WNDCLASSA, WS_OVERLAPPEDWINDOW, WTS_SESSION_LOCK,
-      WTS_SESSION_UNLOCK,
+      CreateWindowExA, DefWindowProcA, IsWindow, RegisterClassA, CW_USEDEFAULT,
+      PBT_APMRESUMESUSPEND, PBT_APMSUSPEND, WINDOW_EX_STYLE, WM_POWERBROADCAST,
+      WM_WTSSESSION_CHANGE, WNDCLASSA, WS_OVERLAPPEDWINDOW, WTS_SESSION_LOCK, WTS_SESSION_UNLOCK,
     },
   },
 };
@@ -53,22 +51,19 @@ impl Default for PowerMonitor {
   }
 }
 
-unsafe fn create_power_events_listener() -> Result<HWND> {
-  let instance = GetModuleHandleA(None)?;
-  debug_assert!(instance.0 != 0);
+unsafe fn create_power_events_listener() -> std::result::Result<HWND, &'static str> {
+  let instance = GetModuleHandleA(None).unwrap_or_default();
+
   let window_class = s!("__psp_event_listener");
 
-  let wc = WNDCLASSA {
-    hCursor: LoadCursorW(None, IDC_ARROW)?,
+  let wnd_class = WNDCLASSA {
     hInstance: instance,
     lpszClassName: window_class,
-    style: CS_HREDRAW | CS_VREDRAW,
     lpfnWndProc: Some(wndproc),
     ..Default::default()
   };
 
-  let atom = RegisterClassA(&wc);
-  debug_assert!(atom != 0);
+  RegisterClassA(&wnd_class);
 
   let hwnd = CreateWindowExA(
     WINDOW_EX_STYLE::default(),
@@ -85,7 +80,9 @@ unsafe fn create_power_events_listener() -> Result<HWND> {
     None,
   );
 
-  // TODO: check hwnd
+  if !IsWindow(hwnd).as_bool() {
+    return Err("Unable to get valid mutable pointer for CreateWindowEx");
+  }
 
   Ok(hwnd)
 }
